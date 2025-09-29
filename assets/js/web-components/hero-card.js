506ae8860330cb3f04bf01b2089633b1f76c9827
Ala Ghoul Bhiri <bhiri.a@decade.fr>
2025-09-29 19:06:38 +0200

    hero banner scrollable

--- assets/js/web-components/hero-card.js
+++ assets/js/web-components/hero-card.js
@@ -2,10 +2,12 @@ class HeroCard extends HTMLElement {
   connectedCallback() {
     const src = this.getAttribute("src") || "";
     const alt = this.getAttribute("alt") || "";
-    const colSpan = this.getAttribute("col-span") || "4";
+    const width = this.getAttribute("width") || "400";
     const height = this.getAttribute("height") || "56";
     const mdHeight = this.getAttribute("md-height") || height;
-    this.className = `relative lg:col-span-${colSpan} rounded-xl overflow-hidden bg-gray-100 h-${height} md:h-${mdHeight}`;
+    
+    this.className = `relative rounded-xl overflow-hidden bg-gray-100 flex-shrink-0 h-${height} md:h-${mdHeight}`;
+    this.style.width = `${width}px`;
 
     this.innerHTML = `
       <img src="${src}" alt="${alt}" class="w-full h-full object-cover"/>
--- assets/js/web-components/nav_item-category.js
+++ assets/js/web-components/nav_item-category.js
@@ -8,7 +8,7 @@ class navItemCategory extends HTMLElement {
             <li>    
                 <a href="${href}" class="inline-flex items-center gap-3 px-4 py-2 rounded-full bg-gray-50 border border-gray-100 shadow-sm text-sm hover:shadow-md focus:outline-none focus:ring-2">
                     <img src="${imgSrc}" alt="${imgAlt}" class="h-6 w-6 rounded-full"/>
-                    <span>${label}</span>
+                    <span class="font-inter font-semiBold hidden lg:inline text-sm text-[#0D0C0D]" >${label}</span>
                 </a>
             </li>
         `;
